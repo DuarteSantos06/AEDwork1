81d895de6c9a56fabf670610850a86d0c9e7a55e
Rodrigo Marcelino <rsmarcelinoo@gmail.com>
2025-10-30 20:58:37 +0000

    Implement capacity management in Services interface
    Added methods to manage service capacity and current occupancy.

--- src/Package/Services/ServicesWithCapacityInterface.java
+++ src/Package/Services/ServicesWithCapacityInterface.java
@@ -1,13 +1,35 @@
 package Package.Services;
-
 import Package.Students.Students;
 import dataStructures.DoublyLinkedList;
 
+/**
+ * Interface representing a service with capacity constraints.
+ * This extends the basic ServicesInterface to add functionality for services
+ * that have a maximum capacity and can track current occupancy (e.g., lodging, eating areas).
+ */
 public interface ServicesWithCapacityInterface extends ServicesInterface {
-
+    
+    /**
+     * Returns the maximum capacity of this service.
+     * This represents the total number of students that can use this service simultaneously.
+     * 
+     * @return the maximum capacity
+     */
     int getCapacity();
-
+    
+    /**
+     * Returns the current number of students occupying this service.
+     * This value should always be less than or equal to the maximum capacity.
+     * 
+     * @return the current occupation count
+     */
     int getCurrentOccupation();
-
+    
+    /**
+     * Returns a list of all students currently at this service.
+     * The list maintains the order in which students arrived at the service.
+     * 
+     * @return a doubly linked list of students currently using this service
+     */
     DoublyLinkedList<Students> getStudents();
 }
